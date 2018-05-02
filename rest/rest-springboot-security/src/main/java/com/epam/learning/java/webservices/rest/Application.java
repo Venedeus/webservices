4dda27c7e115ff0326ec761fdbe8baf08ad08f70
Evgeniy <venedey.e@gmail.com>
2018-05-02 18:05:34 +0300

    rest rest/rest-springboot rest/rest-springboot-security modules added

---
+++ rest/rest-springboot-security/src/main/java/com/epam/learning/java/webservices/rest/Application.java
@@ -0,0 +1,14 @@
+package com.epam.learning.java.webservices.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Катюша on 30.04.2018.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
