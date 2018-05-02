68c897068805cb34116498e84aaacc43e3cddd64
Evgeniy <venedey.e@gmail.com>
2018-05-02 17:52:49 +0300

    soap/soap-springboot module added

---
+++ soap/soap-springboot/src/main/java/com/epam/learning/webservices/soap/CountryRepository.java
@@ -0,0 +1,46 @@
+package com.epam.learning.webservices.soap;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.Currency;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CountryRepository {
+    private static final Map<String, Country> countries = new HashMap<>();
+
+    @PostConstruct
+    public void initData(){
+        Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+        spain.setCurrency(Currency.EUR);
+        spain.setPopulation(46704314);
+
+        Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+        poland.setCurrency(Currency.PLN);
+        poland.setPopulation(38186860);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setCurrency(Currency.GBP);
+        uk.setPopulation(63705000);
+
+        countries.put(spain.getName(), spain);
+        countries.put(poland.getName(), poland);
+        countries.put(uk.getName(), uk);
+    }
+
+    public Country findCountry(String name){
+        Assert.notNull(name, "The country's name must not be null.");
+
+        return countries.get(name);
+    }
+}
